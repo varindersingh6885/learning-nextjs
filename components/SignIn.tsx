24328852a760d63d13a556b8914f98c05484d17e
Varinder Singh <varindersingh6885@gmail.com>
2024-08-28 21:36:50 +0530

    chore: client vs server side components

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,6 +1,8 @@
 import { LabelledInput } from "./LabelledInput";
+import { SignInHandler } from "./SignInHandler";
 
 export const SignInComponent = () => {
+  console.log("hello rendered server side");
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -22,12 +24,7 @@ export const SignInComponent = () => {
                 type={"password"}
                 placeholder="123456"
               />
-              <button
-                type="button"
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              >
-                Sign in
-              </button>
+              <SignInHandler />
             </div>
           </div>
         </a>
