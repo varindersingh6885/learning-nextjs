154c8dc99c825868f079250c7764c0907eaf4389
Varinder Singh <varindersingh6885@gmail.com>
2024-08-28 22:22:00 +0530

    chore: async components / fetch data on server side

---
+++ app/user/page.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+async function getUserDetails() {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  return response.data;
+}
+
+// ! async components
+export default async function UserPage() {
+  const userDetails = await getUserDetails();
+  console.log("user details fetched on server side", userDetails);
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userDetails?.name}</div>
+
+          {userDetails?.email}
+        </div>
+      </div>
+    </div>
+  );
+}
