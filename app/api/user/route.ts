744e906f1ff4675a8446917880baeec1c9bda4e9
Varinder Singh <varindersingh6885@gmail.com>
2024-08-29 22:38:53 +0530

    chore: connect prisma, add user details

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,7 @@
 import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 // handle Get request for route 'api/user'
 export function GET() {
@@ -11,7 +14,18 @@ export function GET() {
 export async function POST(req: NextRequest) {
   const body = await req.json();
   console.log("req body", body);
+
+  const user = await client.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
+
   return Response.json({
-    message: "you got it",
+    userId: user.id,
+    username: user.username,
+    password: user.password,
+    message: "user created",
   });
 }
